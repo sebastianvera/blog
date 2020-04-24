9853854362f1a45e758b304cbd0c572f3755344f
Sebastian Vera <u.verainf@gmail.com>
2020-04-24 02:20:15 +0200

    fix: update author summary

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Sebastián Vera`,
     author: {
       name: `Sebastián Vera`,
-      summary: `Software Developer actualmente viviendo en Berlin y trabajando como Fullstack Developer en `,
+      summary: `Software Developer actualmente viviendo en Berlin y trabajando en `,
     },
     description: `Personal blog by Sebastian Vera.`,
     siteUrl: `https://blog.apipath.io`,
