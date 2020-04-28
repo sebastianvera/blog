50687d84d50c42826cb2aff5ca5052dd9522e1d9
Sebastian Vera <u.verainf@gmail.com>
2020-04-29 01:33:47 +0200

    feat: translate 404 page to spanish

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,8 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>¡Changos!</h1>
+      <p>La página que buscas no existe :(</p>
     </Layout>
   )
 }
